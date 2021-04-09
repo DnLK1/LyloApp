f12be1deeb06ae365f44949306e7543727b52c10
DnLK1 <danielkaneko01@gmail.com>
2021-04-09 04:44:55 -0300

    Dynamic LIKE button

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+//Side menu animation on mobile
+
 const sideMenu = $(".side-menu")
 const toggleSideMenu = $(".header__mobile-menu")
 const closeSideMenu = $(".side-menu__close-icon")
@@ -20,3 +22,37 @@ closeSideMenu.click(function() {
         height: 'auto'
     });
 })
+
+//Like button interaction
+
+const likeSection = $(".feed__post___review--like-section")
+
+function switchLikedToggle() {
+    
+    const findLikeID = $(this).children("img").each(function() {
+        $(this).attr("id")
+    })
+
+    findLikeID.toggle()
+
+
+    const findLikedCount = $(this).children("p").text()
+    const findLikedStatus = $(this).children("p").attr("class")
+    const LikedStatus = $(this).children("p")
+
+    if (findLikedStatus == "unliked") {
+        const likedCountPlusOne = parseInt(findLikedCount) + 1
+
+        JSON.stringify($(this).children("p").text(likedCountPlusOne))
+
+        LikedStatus.toggleClass("unliked")
+    }else{
+        const likedCountMinusOne = parseInt(findLikedCount) - 1
+
+        JSON.stringify($(this).children("p").text(likedCountMinusOne))
+
+        LikedStatus.toggleClass("unliked")
+    }
+}
+
+likeSection.click(switchLikedToggle)
--- README.txt
+++ README.txt
@@ -3,9 +3,9 @@ Independent study project of online Beauty Saloon appointment site.
 Future features:
 
 1) Feed bar with features: post and filter
-2) Dynamic "like" and "comment" buttons
+2) Dynamic "like" button // DONE
 3) Dynamic side menu for mobile version // DONE
 4) Better image positioning on feed
-5) Dynamic "add comment" feature on feed posts
+5) Dynamic "comment" feature on feed posts
 6) Dynamic "infinite scroll" feed
 7) Other pages from nav bar
